b74e392247b5477d1f14f1a4c44a6054c012ecbe
KarinaSinceva <sinceva98@mail.ru>
2022-01-25 15:15:14 +0200

    Initial Commit

---
+++ ColorSlider/ColorSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderApp.swift
+//  ColorSlider
+//
+//  Created by Karina Sintceva on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
